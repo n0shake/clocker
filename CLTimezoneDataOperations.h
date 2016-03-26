62a358a0bafa19012168bde4794fdefb8e77eca2
Abhishek Banthia <abhishekbanthia@me.com>
2016-03-26 15:58:32 -0400

    New data handler class.

---
+++ CLTimezoneDataOperations.h
@@ -0,0 +1,13 @@
+//
+//  CLTimezoneDataOperations.h
+//  Clocker
+//
+//  Created by Abhishek Banthia on 2/9/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CLTimezoneDataOperations : NSObject
+
+@end
---
+++ CLTimezoneDataOperations.m
@@ -0,0 +1,13 @@
+//
+//  CLTimezoneDataOperations.m
+//  Clocker
+//
+//  Created by Abhishek Banthia on 2/9/16.
+//
+//
+
+#import "CLTimezoneDataOperations.h"
+
+@implementation CLTimezoneDataOperations
+
+@end
