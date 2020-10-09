e42da8747022355228714431266975664b06a90f
Abhishek <abhishekbanthia@me.com>
2020-10-09 16:38:09 -0500

    Update tests name.

--- Clocker/ClockerUnitTests/TimezoneDataEqualityTests.swift
+++ Clocker/ClockerUnitTests/TimezoneDataEqualityTests.swift
@@ -1,11 +1,11 @@
 // Copyright © 2015 Abhishek Banthia
 
+import Foundation
+
 @testable import Clocker
 import XCTest
 
-class RateTests: XCTestCase {
-    let rateController = RateController.applicationDidLaunch(UserDefaults())
-
+class TimezoneDataEqualityTests: XCTestCase {
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -13,5 +13,15 @@ class RateTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testEqualityWhenTimezones() {
+        let timezone1 = TimezoneData()
+        timezone1.setLabel(CLEmptyString)
+        timezone1.timezoneID = metaInfo.0.name
+        timezone1.formattedAddress = TimeZone.system.identifer
+        timezone1.selectionType = .timezone
+
+        XCTAssert(timezone1 != nil)
     }
 }
