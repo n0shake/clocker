c425981e66ed49c9089a5f096ba6e4def9a4fbd0
Abhishek <abhishekbanthia@me.com>
2020-10-09 16:50:58 -0500

    Cleanup boilerplate.

--- Clocker/ClockerUnitTests/TimezoneDataEqualityTests.swift
+++ Clocker/ClockerUnitTests/TimezoneDataEqualityTests.swift
@@ -6,14 +6,6 @@ import Foundation
 import XCTest
 
 class TimezoneDataEqualityTests: XCTestCase {
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
 
     func testEqualityWhenTimezoneIdentifiersDiffer() {
         let timezone1 = TimezoneData()
