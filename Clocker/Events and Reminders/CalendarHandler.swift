de044b336038f4b47be9431c48a3253cb3a692bc
Abhishek <abhishekbanthia@me.com>
2022-06-28 15:45:44 -0400

    Exclude events past their 5 minutes mark.

--- Clocker/Events and Reminders/CalendarHandler.swift
+++ Clocker/Events and Reminders/CalendarHandler.swift
@@ -124,7 +124,7 @@ extension EventCenter {
         let relevantEvents = filteredEvents[autoupdatingCalendar.startOfDay(for: Date())] ?? []
 
         let filteredEvents = relevantEvents.filter {
-            $0.event.isAllDay == false && $0.event.endDate.timeIntervalSinceNow > 0
+          $0.event.isAllDay == false && $0.event.endDate.timeIntervalSinceNow > 0 && $0.event.startDate.timeIntervalSinceNow > -300
         }
 
         if filteredEvents.count == 1 { return filteredEvents.first }
